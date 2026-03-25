7f402f3d1709e2b9683dbd534a0f98d9f7cf3ebe
Mang Ogik <pradanayogi123@gmail.com>
2026-03-26 01:16:55 +0800

    update ci/cd install composer packages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('url', function ($app) {
+            $routes = $app['router']->getRoutes();
+            $app->instance('routes', $routes);
+
+            // Ensure we have a request instance, even if it's a dummy for CLI
+            $request = $app->bound('request') ? $app['request'] : \Illuminate\Http\Request::create('/');
+
+            return new \Illuminate\Routing\UrlGenerator(
+                $routes, $request, $app['config']['app.asset_url']
+            );
+        });
     }
 
     /**
